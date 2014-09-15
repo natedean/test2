cc7975f99842703e796c54ccb5612fe5c7ebbba9
Nate <natedeanmail@gmail.com>
2014-09-15 16:33:14 -0500

    Added Cloud Code

--- cloud/main.js
+++ cloud/main.js
@@ -14,3 +14,44 @@ Parse.Cloud.beforeSave(Parse.User, function(request, response) {
 });
 
 
+Parse.Cloud.define("stcAdd", function(request, response){
+  var currentUser = Parse.User.current();
+  currentUser.increment('stcScore', request.params.amount);
+  currentUser.save().then(function(){
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query(Parse.User);
+    query.select("username","stcScore");
+    query.descending("stcScore");
+    return query.find();
+  }).then(function(results){  
+    response.success(results);
+  },function(error){
+    console.log(error.message);
+  });
+  
+});
+
+//Parse.Cloud.define("stcLeaderboard", function(request, response){
+//  Parse.Cloud.useMasterKey();
+//  var query = new Parse.Query(Parse.User);
+//  query.select("username","stcScore");
+//  query.descending("stcScore");
+//  query.find().then(function(results){
+//    console.log("Success");
+//    console.log(results);
+//    response.success(results);
+//  },function(error){
+//    response.error(error);
+//  });
+  
+  
+//  query.limit(10);
+//  query.find().then(function(results){
+//    results.forEach(function(user){
+//      console.log(user.get("stcScore"));
+//    });
+//    response.success();
+//  },function(error){
+//    console.log(error.message);
+//  });
+//});
