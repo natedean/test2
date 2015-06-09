2ab78fdf92fe6a7cf820609d6d5ec799bf1f1a8d
natedean <nate.dean@vml.com>
2015-06-08 23:14:27 -0500

    JSPM test build

--- config.js
+++ config.js
@@ -14,3 +14,20 @@ System.config({
   }
 });
 
+System.config({
+  "map": {
+    "angular": "github:angular/bower-angular@1.4.0",
+    "bootstrap": "github:twbs/bootstrap@3.3.4",
+    "lodash": "npm:lodash@3.9.3",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "github:twbs/bootstrap@3.3.4": {
+      "jquery": "github:components/jquery@2.1.4"
+    },
+    "npm:lodash@3.9.3": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
+
