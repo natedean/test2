358a45fb4a8c296f5268b38b49d28ff80a131f19
natedean <nate.dean@vml.com>
2015-08-24 00:01:14 -0500

    Moved question serving from client to server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ var router = require('koa-router')();
 
 //router.get('/', require('./home'));
 router.get('/test', require('./test'));
+router.get('/question', require('./question'));
 
 module.exports = router;
